3af09168e39a5b3257bfbce79cee2c38b2ac5536
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-02-28 23:49:36 -0300

    Reject notes with empty content

--- routes/notes.ts
+++ routes/notes.ts
@@ -12,6 +12,7 @@ router.get('/', (req, res) =>
 // Get one note
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
+
   noteService.read().then((notes) => {
     const note = notes.find((n) => n.id === Number(id));
     if (!note) return next();
@@ -19,29 +20,53 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
+const generateId = (notes: NoteInterface[]) => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 // Create note
 router.post('/', (req, res) => {
-  const { content, important } = req.body;
+  const { body } = req;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing',
+    });
+  }
+
   noteService.read().then((notes) => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-    const newNote: NoteInterface = {
-      id: maxId + 1,
+    const note: NoteInterface = {
+      id: generateId(notes),
+      content: body.content,
       date: new Date().toUTCString(),
-      content,
-      important,
+      important: body.important || false,
     };
-    if (!isNote(newNote)) return res.sendStatus(400);
-    noteService.save(notes.concat(newNote)).then(() => res.json(newNote));
+
+    if (!isNote(note)) {
+      return res.sendStatus(400);
+    }
+
+    noteService.save(notes.concat(note)).then(() => res.json(note));
   });
 });
 
 // Update note
 router.put('/:id', (req, res) => {
   const { id } = req.params;
+
   noteService.read().then((notes) => {
     const index = notes.findIndex((n) => n.id === Number(id));
-    if (!notes[index]) return res.sendStatus(404);
-    notes[index] = { ...notes[index], important: !notes[index].important };
+
+    if (!notes[index]) {
+      return res.sendStatus(404);
+    }
+
+    notes[index] = {
+      ...notes[index],
+      important: !notes[index].important,
+    };
+
     noteService.save(notes).then(() => res.json(notes[index]));
   });
 });
@@ -49,6 +74,7 @@ router.put('/:id', (req, res) => {
 // Delete note
 router.delete('/:id', (req, res, next) => {
   const { id } = req.params;
+
   noteService.read().then((notes) => {
     const filteredNotes = notes.filter((n) => n.id !== Number(id));
     noteService.save(filteredNotes).then(() => res.sendStatus(204));
