d7ebf632216895a41b3e26130bc78413b659e746
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-02-28 23:07:55 -0300

    Handle cors

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import notesRouter from './routes/notes';
 
 const app = express();
 const port = 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Homepage'));
 app.use('/api/notes', notesRouter);
 app.get('*', (req, res) => res.sendStatus(404));
 
-app.listen(port, () =>
-  console.log(`Server running on port ${port}`),
-);
+app.listen(port, () => console.log(`Server running on port ${port}`));
