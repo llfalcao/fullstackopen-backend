a6ee67ff7817468e4a647b9ac763c2cb5ebf1868
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2022-03-01 15:06:12 -0300

    Use morgan for logging

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import notesRouter from './routes/notes';
 import personRouter from './routes/persons';
 import infoRouter from './routes/info';
@@ -10,10 +11,13 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 
+morgan.token('body', (req: express.Request) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status - :response-time ms :body'));
+
 app.get('/', (req, res) => res.send('Homepage'));
 app.use('/info', infoRouter);
 app.use('/api/notes', notesRouter);
 app.use('/api/persons', personRouter);
-app.get('*', (req, res) => res.sendStatus(404));
+app.use((req, res) => res.sendStatus(404));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
