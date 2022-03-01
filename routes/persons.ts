4b6984a01e1f5fdc23226ab8b00736ca7301b600
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2022-03-01 11:27:18 -0300

    Display information for a single phonebook entry

--- routes/persons.ts
+++ routes/persons.ts
@@ -3,8 +3,19 @@ import personService from '../services/persons';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  personService.read().then((persons) => res.json(persons));
+// Get all 'persons'
+router.get('/', (req, res) =>
+  personService.read().then((persons) => res.json(persons)),
+);
+
+// Get one person
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  personService.read().then((persons) => {
+    const person = persons.find((p) => p.id === Number(id));
+    if (!person) return res.sendStatus(404);
+    res.json(person);
+  });
 });
 
 export default router;
