39803e207d8b181943f06b418b0fe060ef9d1aa8
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-03-02 22:13:43 -0300

    Handle put requests

--- routes/persons.ts
+++ routes/persons.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import personService from '../services/persons';
 import PersonInterface, { isPerson } from '../models/Person';
 
@@ -19,29 +19,66 @@ router.get('/:id', (req, res) => {
   });
 });
 
-// Create person
-router.post('/', (req, res) => {
+const handleMissingInfo = (req: Request, res: Response, next: NextFunction) => {
   const { name, number } = req.body;
+
   if (!name) {
-    return res.status(400).json({ error: 'name missing' });
+    return res.status(400).json({
+      error: 'name missing',
+    });
   }
+
   if (!number) {
-    return res.status(400).json({ error: 'number missing' });
+    return res.status(400).json({
+      error: 'number missing',
+    });
   }
+
+  next();
+};
+
+// Create person
+router.post('/', handleMissingInfo, (req, res) => {
+  const { name, number } = req.body;
 
   personService.read().then((persons) => {
     const isDuplicate = persons.some((p) => p.name === name);
     if (isDuplicate) {
-      return res.status(409).json({ error: 'name must be unique' });
+      return res.status(409).json({
+        error: 'name must be unique',
+      });
     }
 
     const id = Math.floor(Math.random() * 1000);
     const person: PersonInterface = { id, name, number };
     if (!isPerson(person)) {
-      return res.status(400).json({ error: 'invalid value type' });
+      return res.status(400).json({
+        error: 'invalid value type',
+      });
     }
 
     personService.save(persons.concat(person)).then(() => res.json(person));
+  });
+});
+
+// Update person
+router.put('/:id', handleMissingInfo, (req, res) => {
+  const { id } = req.params;
+  const { name, number } = req.body;
+
+  personService.read().then((persons) => {
+    const index = persons.findIndex((p) => p.id === Number(id));
+    if (!persons[index]) return res.sendStatus(404);
+
+    persons[index] = { ...persons[index], name, number };
+
+    if (!isPerson(persons[index])) {
+      return res.status(400).json({
+        error: 'invalid value type',
+      });
+    }
+
+    personService.save(persons).then(() => res.json(persons[index]));
   });
 });
 
