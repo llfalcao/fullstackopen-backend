fb1682b2824f3a56127a8dc83c941b6d67e5a3ef
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-03-08 19:44:52 -0300

    Filter json response from mongodb

--- models/Person.ts
+++ models/Person.ts
@@ -11,6 +11,14 @@ const personSchema = new Schema({
   number: String,
 });
 
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject._v;
+  },
+});
+
 export const PersonModel = model('Person', personSchema);
 
 export const isPerson = (object: any): object is Person =>
