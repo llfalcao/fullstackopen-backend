ea2eca1bfaeec8045a4b0f63e579206a06bc1c95
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-03-21 20:22:23 -0300

    Add more tests

--- tests/average.test.ts
+++ tests/average.test.ts
@@ -4,3 +4,13 @@ test('average of array', () => {
   const result = average([1, 2, 3, 4, 5]);
   expect(result).toBe(3);
 });
+
+test('average of array', () => {
+  const result = average([5, 10, 15, 20, 25, 30]);
+  expect(result).toBe(17.5);
+});
+
+test('average of array', () => {
+  const result = average([2, 64, 578, 597, 52, 6]);
+  expect(result).toBe(216.5);
+});
