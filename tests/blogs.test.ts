f54f57435de9c259bcb168bbe792c3508c927e00
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-03-22 18:57:23 -0300

    Test favorite blog

--- tests/blogs.test.ts
+++ tests/blogs.test.ts
@@ -32,11 +32,8 @@ describe('total likes', () => {
 test('favorite blog', () => {
   const result = listHelper.favoriteBlog(listWithMultipleBlogs);
   expect(result).toEqual({
-    __v: 0,
-    _id: '5a422b3a1b54a676234d17f9',
     author: 'Edsger W. Dijkstra',
     likes: 12,
     title: 'Canonical string reduction',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
   });
 });
--- utils/list_helper.ts
+++ utils/list_helper.ts
@@ -6,12 +6,15 @@ const dummy = (blogs: Blog[]) => 1;
 const totalLikes = (blogs: Blog[]) =>
   blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
-const favoriteBlog = (blogs: Blog[]) =>
-  blogs.reduce((fav, blog) => {
-    if (blog.likes > fav.likes) {
-      return blog;
-    } else return fav;
-  }, blogs[0]);
+const favoriteBlog = (blogs: Blog[]) => {
+  const favorite = blogs.reduce(
+    (fav, blog) => (blog.likes > fav.likes ? blog : fav),
+    blogs[0],
+  );
+
+  const { title, author, likes } = favorite;
+  return { title, author, likes };
+};
 
 const listHelper = { dummy, favoriteBlog, totalLikes };
 export default listHelper;
