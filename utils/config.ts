92dda4bb6bdf2f8573fdb8cb3860adaa4e07304e
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-03-23 21:10:18 -0300

    Use different databases for development and production

--- utils/config.ts
+++ utils/config.ts
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+declare const process: {
+  env: {
+    NODE_ENV: string;
+    MONGODB_URI: string;
+    TEST_MONGODB_URI: string;
+    PORT: number;
+  };
+};
+
 const PORT = process.env.PORT || 8080;
-const MONGODB = process.env.MONGODB as string;
+const MONGODB =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 const config = { PORT, MONGODB };
 export default config;
