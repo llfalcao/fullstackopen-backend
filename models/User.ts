bd849f93cc702411473c487f824d52ff8c543716
Luiz Falcao <luizotavio-falcao@hotmail.com>
2022-04-06 22:41:21 -0300

    Create user model

---
+++ models/User.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+import { Note } from './Note';
+
+export interface User {
+  username: string;
+  name: string;
+  passwordHash: string;
+  notes: Note[];
+}
+
+const UserSchema = new Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Note',
+    },
+  ],
+});
+
+UserSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+export const UserModel = model('User', UserSchema);
