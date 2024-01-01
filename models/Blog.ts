971597c427bacd945987b5bb689a6918eb56e04a
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2024-01-01 18:25:00 -0300

    feat(blogs): clean up some user info returned with the blogs

--- models/Blog.ts
+++ models/Blog.ts
@@ -32,10 +32,11 @@ const blogSchema = new Schema({
 });
 
 blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
+  transform: (_, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    delete returnedObject.user.blogs;
   },
 });
 
